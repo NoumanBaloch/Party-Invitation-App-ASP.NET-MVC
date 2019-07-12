ae22df02d836c8b1661934625bebd67f0967f33b
mrnoumanbaloch@gmail.com <mrnoumanbaloch@gmail.com>
2019-07-12 16:49:50 +0500

    Initial Commit

---
+++ PartyInvite/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using PartyInvite.Models;
+using System;
+using System.Web.Mvc;
+namespace PartyInvite.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greating = hour < 12 ? "Good Morning" : "Good Evening";
+            return View();
+        }
+        [HttpGet]
+        public ActionResult RSVPForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult RSVPForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+
+    }
+}
---
+++ PartyInvite/Models/GuestResponse.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvite.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter yoru name.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter your email address.")]
+        [RegularExpression(".+\\@.+\\..+",
+        ErrorMessage = "Please enter a valid email address")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter your phone number.")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Please specify weather you I'll attend the party.")]
+        public bool? WillAttend { get; set; }
+    }
+}
